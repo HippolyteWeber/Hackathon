b4db15cc71c0ab33605dd7dd4f7aaad400e8dda0
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-05-16 12:29:49 +0200

    first commit

---
+++ src/data/teamMembers.js
@@ -0,0 +1,58 @@
+const teamMembers = [
+  {
+    id: 1,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "Aurelien Chetot",
+    desc: "",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 2,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "Jebri Mohamed",
+    desc: ".",
+    linkedin: "https://www.linkedin.com/in/mohamed-jebri-3a11047b/",
+    github: "https://github.com/memdjjebri",
+  }
+  {
+    id: 3,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "Hippolyte Weber",
+    desc: "",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 4,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "Zainouddine Riday",
+    desc: "",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 5,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "Pierre Juppin",
+    desc: "An emerging talent from Wild Code School enthusiastically juggles between lines of code, building his path towards excellence in the world of development. Armed with an unwavering passion for technology and boundless determination, he forges expertise through challenging projects and intensive training. He embodies the adventurous spirit and insatiable curiosity that propel innovators towards new horizons",
+    linkedin: "https://www.linkedin.com/in/baptiste-cazenave-9384b3209/",
+    github: "https://github.com/baptistesss",
+  },
+  {
+    id: 6,
+    image:
+      "https://cdn.pixabay.com/photo/2021/02/10/15/14/lake-6002433_1280.jpg",
+    name: "",
+    desc: "An emerging talent from Wild Code School enthusiastically juggles between lines of code, building his path towards excellence in the world of development. Armed with an unwavering passion for technology and boundless determination, he forges expertise through challenging projects and intensive training. He embodies the adventurous spirit and insatiable curiosity that propel innovators towards new horizons",
+    linkedin: "https://www.linkedin.com/in/baptiste-cazenave-9384b3209/",
+    github: "https://github.com/baptistesss",
+  },
+];
+
+export default teamMembers;
